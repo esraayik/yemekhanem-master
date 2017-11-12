f4a9098cd448eadda7adfb2017908b0f6a4fce3d
Esra <esra@github.com>
2017-11-12 13:55:47 +0300

    something

--- app/src/main/java/com/akillitarifler/yemekhanem/CategoryActivity.java
+++ app/src/main/java/com/akillitarifler/yemekhanem/CategoryActivity.java
@@ -1,13 +1,36 @@
 package com.akillitarifler.yemekhanem;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 public class CategoryActivity extends AppCompatActivity {
+   ImageView deserts;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+
+
+         deserts = (ImageView) findViewById(R.id.deserts);
+
+         deserts.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 Intent i = new Intent(CategoryActivity.this,MenuActivity.class);
+                 startActivity(i);
+
+
+
+             }
+         });
+
     }
+
+
 }
---
+++ app/src/main/java/com/akillitarifler/yemekhanem/MenuActivity.java
@@ -0,0 +1,16 @@
+package com.akillitarifler.yemekhanem;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by esraayik on 12.11.2017.
+ */
+
+public class MenuActivity extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.food);
+    }
+
+}
